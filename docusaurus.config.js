c28f4ef08ec07132d088c8db372b082b292bd8a7
Eike Caldeweyher <hello@eikecaldeweyher.de>
2022-11-18 13:38:35 +0100

    fix:favicon for gh-pages.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   baseUrl: "/",
   onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+  favicon: "favicon.ico",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
