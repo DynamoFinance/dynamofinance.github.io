9bfa93fdf60bbdb540abccd05cf9977329e71a11
Eike Caldeweyher <hello@eikecaldeweyher.de>
2023-07-09 19:25:15 +0200

    feat: add algolia search on documentation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,6 +76,30 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      algolia: {
+        // The application ID provided by Algolia
+        appId: "YHEMCW7E5Q",
+
+        // Public API key: it is safe to commit it
+        apiKey: "e53b5e9617217e58f980162e969ce33f",
+
+        indexName: "dynamofinanceio",
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: "/docs/", // or as RegExp: /\/docs\//
+          to: "/",
+        },
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: "search",
+      },
       navbar: {
         hideOnScroll: true,
         title: " ",
